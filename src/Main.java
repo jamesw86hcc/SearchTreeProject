b0d9260376cebdc7a4719c397eaf7f9816c5f8d6
James Williams <marysa.williams@howardcc.edu>
2023-04-14 19:20:12 -0400

    Added loop in Main

--- src/Main.java
+++ src/Main.java
@@ -13,37 +13,49 @@ public class Main {
 
         try (Scanner fileInput = new Scanner(Paths.get(fileName))) {
             System.out.println("File " + Paths.get(fileName).getFileName() + " accessed.");
+            Boolean searchAgain = true;
 
-            System.out.println("What is the word you would like to search for?: ");
-            String target = userInput.next();
-            target = target.trim().toLowerCase();
+            do {
+                System.out.println("What is the word you would like to search for?: ");
+                String target = userInput.next();
+                target = target.trim().toLowerCase();
 
-            ArrayList<Line> lines = new ArrayList<>();
+                ArrayList<Line> lines = new ArrayList<>();
 
-            for (int i = 0; fileInput.hasNextLine(); i++)
-            {
-                String next = fileInput.nextLine();
-                String[] nextWords = next.replaceAll("[^A-Za-z]", " ").trim().split(" ");
+                for (int i = 0; fileInput.hasNextLine(); i++) {
+                    String next = fileInput.nextLine();
+                    String[] nextWords = next.replaceAll("[^A-Za-z]", " ").trim().split(" ");
 
-                for (String word : nextWords) {
-                    if (word.equals(target)) {
-                        lines.add(new Line(i, next));
+                    for (String word : nextWords) {
+                        if (word.equals(target)) {
+                            lines.add(new Line(i, next));
+                        }
+                    }
+
+                }
+
+                if (lines.isEmpty()) {
+                    System.out.println("There were no matches of '" + target + "'.");
+                } else {
+                    for (Line l : lines) {
+                        System.out.println(l.getNumber() + ": " + l.getContent());
                     }
                 }
 
-            }
+                System.out.println("Would you like to search again? Y/N:");
+                char response = userInput.next().toLowerCase().charAt(0);
 
-            if (lines.isEmpty())
-            {
-                System.out.println("There were no matches of '" + target + "'.");
-            } else
-            {
-                for (Line l: lines){
-                    System.out.println(l.getNumber() + ": " + l.getContent());
+                if (response == 'y')
+                {
+                    searchAgain = true;
+                } else if (response == 'n')
+                {
+                    searchAgain = false;
+                } else
+                {
+                    searchAgain = true;
                 }
-            }
-
-
+            } while (!searchAgain);
         } catch (IOException | NoSuchElementException e) {
             throw new RuntimeException(e);
         }
